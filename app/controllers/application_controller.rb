eee0e7b29e6e6da3dfcb65548a7bf228d4c44167
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-15 15:13:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,4 +41,9 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
+  delete '/posts/:id/delete' do
+    #@post = Post.find(params[:id].to_i)
+    @post.destroy
+    erb :index
+
 end
