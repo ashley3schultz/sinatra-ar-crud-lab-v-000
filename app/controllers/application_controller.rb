7e771dd4e361e9481dba9c4e1fea7f5e165cf08b
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-15 14:47:29 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,6 @@ class ApplicationController < Sinatra::Base
 
   post '/posts' do
     @post = Post.create(name: params[:name], content: params[:content])
-    @post
     erb :index
   end
 
@@ -28,6 +27,7 @@ class ApplicationController < Sinatra::Base
 
   get '/posts/:id' do
     @post = Post.find_by_id(params[:id].to_i)
+    @post
     #erb :show
   end
 
