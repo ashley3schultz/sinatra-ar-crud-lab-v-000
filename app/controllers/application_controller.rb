57029f6f102bc8f8bf3750e9326b5ba0a84c3732
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-15 14:19:58 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,8 +27,7 @@ class ApplicationController < Sinatra::Base
 
   get '/posts/:id' do
     #@post = Post.find_by_id(params[:id].to_i)
-    #erb :show
-    'hello'
+    erb :show
   end
 
   get '/posts/:id/edit' do
