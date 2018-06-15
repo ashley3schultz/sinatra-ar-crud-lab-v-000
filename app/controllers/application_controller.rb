2f03abc3b4e921c9502d2cf65ebef0d451561efe
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-15 14:58:58 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,6 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/posts/:id' do
-    #binding.pry
     @post = Post.find(params[:id].to_i)
     @post.update(name: params[:name], content: params[:content])
     erb :show
